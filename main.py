761ad2221775081e370ebf95fcb65c67b825ac6a
thecr04k <capellanjohnedward@gmail.com>
2024-07-31 11:01:07 +0800

    creation of Index using FastAPI

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
     description="Index of FastAPI"
 )
 async def index():
-    return "HELLO"
+    return "HELLO WORLD"
 
 if __name__ == "__main__":
     uvicorn.run(app=app, host="0.0.0.0", port= 14369)
